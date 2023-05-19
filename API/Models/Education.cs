137dc5e0f961559bf17e729df08261c9789e5cb6
BluB3ry <davidraharja910@gmail.com>
2023-05-19 16:11:02 +0700

    Menambah swagger dan crud
    menambah swagger dan crud untuk masing masing model

--- API/Models/Education.cs
+++ API/Models/Education.cs
@@ -15,9 +15,9 @@ namespace API.Models
         public Guid UniversityGuid { get; set; }
 
         //Cardinality
-        public University University { get; set; }
+        public University? University { get; set; }
 
-        public Employee Employee { get; set; }
+        public Employee? Employee { get; set; }
 
     }
 }
